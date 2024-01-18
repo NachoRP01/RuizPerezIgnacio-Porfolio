9d2b3cd6d934f9a39ef524b0005118eec1007770
nacho <nach.rui.per.01@gmail.com>
2024-01-18 14:12:08 +0100

    Agregado la hamburguesa y parte de pagina lenguajes

---
+++ porfolio/src/app/models/lenguajes.model.ts
@@ -0,0 +1,3 @@
+export class Lenguajes{
+    constructor(public NombreLenguaje:string, public Texto:string, public IgmagenNombre:string){}
+}
---
+++ porfolio/src/app/models/proyectos.model.ts
@@ -0,0 +1,3 @@
+export class Proyecto{
+    constructor(public NombreProyecto:string, public Texto:string){}
+    }
